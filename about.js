d8dc81da3a1d0b7e73e838227bc379f7b40db4ea
Carlotta <aouncarlotta@Submarino-Derretido.local>
2021-04-11 20:22:37 +0100

    remake of landing page

--- about.js
+++ about.js
@@ -1,7 +1,9 @@
+
+
 //hover on bio div but affect overlay text + bio
   $(".bio").hover(function() {
   	$(".bio").addClass("hoveringBio");
-  	$(".overlay_text").addClass("hoveringOverlay");
+  	$(".overlay_text").addClass("hoveringOverlay2");
     // $('.overlay-text').css('color', 'rgba(0,0,0,0');
     // $('.bio').css('text-decoration', 'underline');
   // }, function() {
@@ -12,13 +14,17 @@
 
   $(".bio").mouseleave(function(){
   	$(".bio").removeClass("hoveringBio");
-  	$(".overlay_text").removeClass("hoveringOverlay");
+  	// $(".overlay_text").removeClass("hoveringOverlay");
   });
 
-  $(".overlay_text").hover(function(){
+  $(".overlay_text, .exhibitions").hover(function(){
   	$(".overlay_text").addClass("hoveringOverlay2");
   });
 
-$(".overlay_text").mouseleave(function(){
-	$(".overlay_text").removeClass("hoveringOverlay2");
-});
+  $(".overlay_text").mouseleave(function(){
+    $(".overlay_text").removeClass("hoveringOverlay2");
+  })
+
+// $(".overlay_text").mouseleave(function(){
+// 	$(".overlay_text").removeClass("hoveringOverlay2");
+// });
